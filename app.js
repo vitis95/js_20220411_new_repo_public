a8a96205dddb6ea0f19fd3dafb134fdbb2e869fb
vitis95 <37616023+vitis95@users.noreply.github.com>
2022-04-26 23:24:24 +0200

    scroll page header + hamburger menu

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
 // tutorial https://www.youtube.com/watch?v=3PHXvlpOkf4&t=421s 
 // min 20:27
 
-
+// Start // Es JS Color Flipper Simple
 const colors = ["#2B2D42", "#8D99AE", "#EDF2F4", "#EF233C", "#D90429"];
-
-
 const btn = document.getElementById('btn');
 const color = document.querySelector('.color');
 const box_colored = document.getElementById('box-colored');
 const box_colored_text = document.getElementById('box-colored-text');
-
-
 
 btn.addEventListener("click", function () {
     // get random number between 0 and 3 - serve per pescare un coore a caso dall'array sopra. 
@@ -28,38 +24,33 @@ btn.addEventListener("click", function () {
     box_colored.style.backgroundColor = colors[randomNumber];
   });
   
-
-
 function getRandomNumber() {
     var prova = Math.floor(Math.random() * colors.length);
     return prova;
 }
+// End // Es JS Color Flipper Simple
 
 
+// Start // Action Scroll Page, add class header-fixex-scroll
+$('document').ready(function(){
+$(window).scroll(function(){
+    var scroll = $(window).scrollTop(); 
+    if (scroll >= 1) {
+        console.log("CIAO");
+        $("#header-scroll").addClass("header-fixed-scroll");
+    } else {
+        $("#header-scroll").removeClass("header-fixed-scroll");
+    }
+});
+});
+// End // Action Scroll Page, add class header-fixex-scroll
 
 
-// $('document').ready(function(){
-    
-// $(window).scroll(function(){
-// $("#header-scroll").toggleClass("header-fixed-scroll");
-// });
-// });
-
-// window.addEventListener('scroll', function() {
-//     console.log("entro 3");
-    
-
-//     // document.getElementById("header-scroll").add("header-fixed-scroll");
-
-// });
-
-// $(document).ready(function() {
-//     $(window).scroll(function () {
-//         var scroll = $(window).scrollTop();
-//         if (scroll > 250) {
-//             $(".header-fixed").addClass("resized");
-//         } else {
-//             $(".header-fixed").removeClass("resized");
-//         }
-//     });
-// });
+// Start // Hamburger Menu
+$(document).ready(function(){
+    $(".nav-icon-bar").click(function(e){
+        $(".nav-links").toggleClass('show-nav-mobile');
+        e.preventDefault();
+    });
+});
+// End // Hamburger Menu
