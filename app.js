58a8e88f6c0ce9916a4347e60bfbf4b680017b8a
vitis95 <37616023+vitis95@users.noreply.github.com>
2022-05-05 23:59:48 +0200

    secondo es hex . js - min 30:27

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-// tutorial https://www.youtube.com/watch?v=3PHXvlpOkf4&t=421s 
+// tutorial https://www.youtube.com/watch?v=3PHXvlpOkf4&t=421s
 // min 20:27
 
 // Start // Es JS Color Flipper Simple
 const colors = ["#2B2D42", "#8D99AE", "#EDF2F4", "#EF233C", "#D90429"];
 const btn = document.getElementById('btn');
+console.log(btn, "btn");
 const color = document.querySelector('.color');
 const box_colored = document.getElementById('box-colored');
 const box_colored_text = document.getElementById('box-colored-text');
 
 btn.addEventListener("click", function () {
-    // get random number between 0 and 3 - serve per pescare un coore a caso dall'array sopra. 
+    // get random number between 0 and 3 - serve per pescare un coore a caso dall'array sopra.
     const randomNumber = getRandomNumber();
     // console.log(randomNumber);
     //document.body.style.backgroundColor = colors[randomNumber];
-    console.log(colors[randomNumber]);
     if (colors[randomNumber] == "#EDF2F4"){
         console.log("CIAO");
         box_colored_text.style.color = "#1e1e1e";
@@ -23,7 +23,7 @@ btn.addEventListener("click", function () {
     color.textContent = colors[randomNumber];
     box_colored.style.backgroundColor = colors[randomNumber];
   });
-  
+
 function getRandomNumber() {
     var prova = Math.floor(Math.random() * colors.length);
     return prova;
@@ -34,7 +34,7 @@ function getRandomNumber() {
 // Start // Action Scroll Page, add class header-fixex-scroll
 $('document').ready(function(){
 $(window).scroll(function(){
-    var scroll = $(window).scrollTop(); 
+    var scroll = $(window).scrollTop();
     if (scroll >= 1) {
         $("#header-scroll").addClass("header-fixed-scroll");
     } else {
--- hex.js
+++ hex.js
@@ -1,22 +1,28 @@
-// tutorial https://www.youtube.com/watch?v=3PHXvlpOkf4&t=421s 
+// tutorial https://www.youtube.com/watch?v=3PHXvlpOkf4&t=421s
 // min 27
 
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 
-const btn = document.getElementById("btn");
-const color = document.querySelector(".color");
+const btn2 = document.getElementById("btn2");
+const color2 = document.querySelector(".color-2");
+const box_colored2 = document.getElementById('box-colored-2');
+const box_colored_text2 = document.getElementById('box-colored-text');
+console.log(btn2);
+console.log(color2, "color2");
 
-console.log(btn);
-console.log(color);
 
-
-btn.addEventListener("click", function () {
+btn2.addEventListener("click", function () {
+  console.log("debtro");
     let hexColor = "#";
     for (let i = 0; i < 6; i++) {
-        hexColor += hex[0]
-        console.log(hexColor); 
+        hexColor += hex[getRandomNumber()];
+        console.log(hexColor,"ciao");
     }
-    color.textContent = hexColor;
-    ox_colored.style.backgroundColor = hexColor;
+    color2.textContent = hexColor;
+    box_colored2.style.backgroundColor = hexColor;
     // document.body.style.backgroundColor = hexColor;
 });
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * hex.length);
+}
